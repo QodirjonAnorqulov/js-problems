20019bef8154219770eec8e869443467f44e27c4
qodirjonanorqulov@gmail.com <qodirjonanorqulov@gmail.com>
2022-09-14 14:20:52 +0500

    problems js

---
+++ main.js
@@ -0,0 +1,143 @@
+
+      birinchi
+var number = 4
+if(number>0){
+    console.log("bu musbat son");}else{
+        console.log("bu musbat son emas");}
+                   ikkinchi
+    var number =59
+if((number -1)%2==0){
+    console.log("bu toq son");}else{
+        console.log("bu juft son ");}
+               uchinchi
+     var number =54
+     if(number%2==0){
+         console.log("bu juft son");}else{
+             console.log("bu toq son");}
+               to'rtinchi
+     a=9
+     b=6
+     if(a>0 && b>0){
+        console.log("javob to'g'ri");
+     }else{ console.log("javob noto'g'ri");}
+                    beshinchi
+    a=9
+    b=7
+    c=6
+    if(a>b && b>c){
+        console.log("javob to'g'ri");
+    }else{
+        console.log("javob noto'g'ri");
+    }
+                        oltinchi
+    a=10
+    b=15
+    c=20
+    if(a<b && b<c){
+        console.log("javob to'g'ri");
+    }else{
+        console.log();"javob noto'g'ri"
+    }
+                        yettinchi
+    a=1
+    b=2
+    c=3
+    d=4
+    if(a>0 && b>0 && c>0 && d>0){
+        console.log("javobmto'g'ri");
+    }else{
+        console.log("javob noto'g'ri");
+    }
+                        sakkizinchi
+    a=5
+    b=5
+    c=7
+    d=9
+    if(a>=0 && b>=0 && c>=0 && d>=0 ){
+        console.log("javob to'g'ri");
+    }else{
+        console.log("javob noto'g'ri");
+    }
+                        to'qqizinchi
+     a=-1
+    b=5
+    c=7
+    if(a<0 && b>=0 && c>=0 ){
+        console.log("javob to'g'ri");
+    }else{
+        console.log("javob noto'g'ri");
+    }
+                        o'ninchi
+       let days = [
+           "Dushanba",
+           "Seshanba",
+          "Chorshanba",
+           "Payshanba",
+           "Juma",
+          "Shanba",
+           "Yakshanba",
+          ];
+                              let num = prompt("hafta kunlarini kirit:");
+          if (num>0 && num<8) alert(days[num-1]);else alert("hafta kunlari 1 dan 7 gacha")
+                              o'nbir
+      var score =95
+      if(score>=98){
+            console.log("uzmobile");
+      }else if(score >=96){
+            console.log("ums");
+      }else if(score>=93){
+            console.log("ucell");
+      }else if(score>=90){
+            console.log("beeline");
+      } 
+                                    o'nikki
+      let num =-3
+      if(num>0){
+            console.log(num+1);}else
+      {
+            console.log(num-1);
+      }
+                                    o'nto'rt
+      var num =2
+      if(num >0){
+            console.log(num+3);
+      }else{
+            console.log(num-2);
+      }
+                                    o'nto'rt
+      a = -5
+      b = 33
+      if(a<b){
+            console.log("javob to'g'ri");
+      }else{
+            console.log("javob noto'g'ri");
+      }
+
+                              o'nbeshinchi
+      a = 9
+      b = 8
+      c = 9
+      if(
+            a<b && b<c && a<c
+      ){
+            console.log("javob to'g'ri");
+      }else{
+            console.log("javob not'g'ri");
+      }
+                                    o'nolti
+
+            a =3
+            b = 9
+            c = 9
+                                  
+     if(
+           a<b && b>=c && a<c
+     ){
+           console.log("javob to'g'ri");
+     }else{
+           console.log("javob not'g'ri");
+     }
+                                 o'nsakkiz
+      
+  
+    
